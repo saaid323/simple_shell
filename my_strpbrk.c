1c17d37def54f1f5adf830afd298aae999e87e9a
saaid <saaid2538@gmail.com>
2023-05-24 02:31:47 -0700

    added my_strpbrk

---
+++ my_strpbrk.c
@@ -0,0 +1,26 @@
+#include "shell.c"
+
+/**
+ * my_strpbrk - Finds the first occurrence of character in str.
+ * @str: Pointer to the string to be searched.
+ * @delim: Pointer to the string of delimiter characters.
+ * Return: Pointer to str, or NULL if not found.
+ */
+char *my_strpbrk(const char *str, const char *delim)
+{
+	while (*str)
+	{
+		const char *d = delim;
+
+		while (*d)
+		{
+			if (*str == *d)
+			{
+				return (char *)str;
+			}
+			d++;
+		}
+		str++;
+	}
+	return (NULL);
+}
